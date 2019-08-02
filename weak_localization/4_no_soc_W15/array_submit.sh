db778ffa3cb14bfdaea7fa997485c3c3a419a6fd
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2019-08-02 11:34:08 -0700

    standard commit

---
+++ weak_localization/4_no_soc_W15/array_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --array=0-27
+#SBATCH -p nodes # partition (queue)
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
+#SBATCH --mem 16000 # memory pool for all cores
+#SBATCH -t 2-20:00 # time (D-HH:MM)
+#SBATCH -o message.out # STDOUT
+#SBATCH -e error.err # STDERR
+export OMP_NUM_THREADS=1
+module purge
+module add anaconda3
+python3 socconductance.py $SLURM_ARRAY_TASK_ID
