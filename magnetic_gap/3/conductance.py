02fab6ea5158660e350d8a557cb73d962d2840cb
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2019-08-08 15:28:02 -0700

    standard commit

--- magnetic_gap/3/conductance.py
+++ magnetic_gap/3/conductance.py
@@ -57,7 +57,7 @@ phi = np.pi
 mu = (C0 - C1 * M0 / M1) #- 7e-3# tune chem pot to Dirac nodes
 W = 0.0 # disorder strength
 m=0#0.006
-Lx = 12
+Lx = 22
 Ly = 7
 
 xdir = 0 #set direction of transport to one
