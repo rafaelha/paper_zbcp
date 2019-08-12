9360125bf52ef502ffbc7f3e247a1a8345974891
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2019-08-11 23:06:34 -0700

    standard commit

--- majorana/18_4/plot2.py
+++ majorana/18_4/plot2.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+from matplotlib.backends.backend_pdf import PdfPages
 
 files = glob.glob('*.pickle')
 
@@ -30,6 +31,7 @@ Reh = np.zeros(l)
 Ree = np.zeros(l)
 N = np.zeros(l)
 G = np.zeros(l)
+pdf = PdfPages('fig_separate.pdf')
 for f in files:
     reader = open(f,'rb')
 
@@ -43,12 +45,15 @@ for f in files:
             duration += pickle.load(reader)
             mu = pickle.load(reader)
 
-            en += pickle.load(reader)
+            en2 = pickle.load(reader)
+            en += en2
 
-            N += pickle.load(reader)
+            N2 = pickle.load(reader)
+            N += N2
             Ree += pickle.load(reader)
             Reh += pickle.load(reader)
-            G += pickle.load(reader)
+            G2 = pickle.load(reader)
+            G += G2
 
             Bx = pickle.load(reader)
             gap = pickle.load(reader)
@@ -56,11 +61,21 @@ for f in files:
             mu_local = pickle.load(reader)
             B1 = pickle.load(reader)
             seed = pickle.load(reader)
+            plt.figure()
+            plt.plot(en2, G2)
+            plt.plot(np.block([-np.flip(en2,0), en2]), np.block([np.flip(G2,0), G2]), label='G')
+            plt.plot(np.block([-np.flip(en2,0), en2]), np.block([np.flip(N2,0), N2]), '-.', label='N')
+            plt.xlabel('Bias in $\mu$eV')
+            plt.ylabel('Conductance <G> ('+str(count)+' realizations)')
+            plt.title('k='+str(Lz))
+            pdf.savefig()
+            plt.close()
 
             count += 1
     except EOFError:
         reader.close()
 
+pdf.close()
 en = en / count * 1e6
 Reh = Reh / count
 Ree = Ree / count
